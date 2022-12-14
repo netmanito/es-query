cdcaf00d051cfea99e9bd03f8a1980a33e514cf9
Jacinto Calvo Sintes <jaci@seriousman.org>
2022-12-14 17:57:02 +0100

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 # set -e
 # set -o pipefail
-set -u
+# set -u
 
-USAGE="Usage: bash install.sh run"
-# EXPECTED?=1
-
-ES_QUERY_DIR="${HOME}/.es-query"
 ES_QUERY="${HOME}/bin/es-query"
-ROOT_DIR="${PWD}"
+ES_QUERY_DIR="${HOME}/.es-query"
+BASH_COMP="${HOME}/.es-query/es-query_bash_completion"
+ENV_FILE="${HOME}/.es-query/.env"
 
-BASH_COMP="$(ls -las "${HOME}"/.es-query/es-query_bash_completion)"
-
-function diffComp {
-        diff es-query_bash_completion "${HOME}"/.es-query/es-query_bash_completion
-}
-
+function env_me {
 ENV_ME=$(
         cat <<EOF >>~/.es-query/.env
         USERNAME="elastic"
@@ -23,70 +16,86 @@ ENV_ME=$(
         URL="localhost:9200"
 EOF
 )
+$ENV_ME
+}
 
-if [ ! -f "$ES_QUERY" ]; then
-        echo "es-query not installed"
-        $ES_QUERY
-        if [ -z "$ES_QUERY_DIR" ]; then
-                if [ "${PWD}" == "${HOME}" ]; then
-                        echo "No .es-query directory found, creating it for you."
-                        echo "..."
-                        mkdir .es-query
-                else 
-                        echo "No .es-query directory found, creating it for you."
-                        echo "..."
-                        mkdir "${HOME}"/.es-query
-                fi
+function checkBC {
+        diff es-query_bash_completion "${HOME}"/.es-query/es-query_bash_completion
+}
+
+function checkDir {
+    if [ -d "$ES_QUERY_DIR" ]; then
+        echo "++++++++++++++++++++++++++++++++++++"
+        echo ".es-query Directory Found"
+        echo "Checking differences in es-query_bash_completion"
+        if [[ -f "$BASH_COMP" ]]; then
+                echo "query_bash_completion found"
+                echo "checking changes"
+                checkBC
+                echo "Do you want to Update?"
+                read -p "Press Enter to continue"
+        else
+                echo "query_bash_completion File does not exist, not comparing"
+        #       cp es-query_bash_completion "${HOME}"/.es-query/
         fi
-        echo "Copying files to directory"
-        if [ ! -f "${HOME}/.es-query/.env" ]; then
-                echo ".env file not found, creating one for you."
-                $ENV_ME
-        else 
-                echo ".env file found, not adding it."
+        echo "++++++++++++++++++++++++++++++++++++"
+        echo "Checking .env file"
+        if [[ ! -f "$ENV_FILE" ]]; then
+                echo ".env file found, doing nothing"
+        else
+                echo ".env file Not found, creating ..."
+                env_me
+                echo "Done"
         fi
-        echo "Installing es-query"
+    else
+        echo "------------------------------------"
+        echo "Checking for .es-query Directory"
+        echo ".es-query directory not found, creating one for you"
+        read -p "Press ENTER to Continue"
+        echo "..."
+        mkdir "${HOME}"/.es-query
+        echo ".es-query directory created in your HOME"
+        echo "======================================================"
+        echo "Copying necessary files to .es-query directory"
+        env_me
+        cp es-query_bash_completion "${HOME}"/.es-query/
+        echo "Done ..."
+        echo ""
+        echo "======================================================"
+        echo ""
+        echo "ES-Query resources installed"
+    fi
+}
+
+if [ ! -f  "$ES_QUERY" ]; then
+        echo "======================================================"
+        echo "ES-Query NOT Installed"
+        read -p "Press ENTER to continue Install or CRTL+C to abort"
+        echo "======================================================"
+        echo "Installing ES-Query"
         cp es-query "${HOME}"/bin/
         chmod +x "${HOME}"/bin/es-query
-        echo "es-query installed"
-        echo "Installing es-query bash completion"
-        if [ -z "$BASH_COMP" ]; then
-                cp es-query_bash_completion "${HOME}"/.es-query/
-        fi
+        echo "ES-Query installed"
+        checkDir
         echo "All Done!!"
 else
-        echo "es-query found on your ~/bin directory"
+        echo "======================================================"
+        echo "ES-Query found on your ~/bin directory"
         echo "Do you want to update?"
-        read -p "Press ENTER to continue"
+        echo ""
+        read -p "Press ENTER to continue or CRTL+C to abort"
+        echo ""
         echo "Updating es-query"
         mv "${HOME}"/bin/es-query{,.old}
         cp es-query "${HOME}"/bin/es-query
         chmod +x "${HOME}"/bin/es-query
-        echo "Checking associated files"
-        if [ -z "$ES_QUERY_DIR" ]; then
-                echo ".es-query directory not found, creating one for you"
-                mkdir "${HOME}"/.es-query
-                echo "Copying files to directory"
-                $ENV_ME
-                cp es-query_bash_completion "${HOME}"/.es-query/
-        else
-                echo ".es-query directory found"
-                echo "Checking differences in es-query_bash_completion"
-                EXISTS=$(diffComp)
-                if [ -z "$EXISTS" ]; then
-                        if [ -z "$BASH_COMP" ]; then
-                        cp es-query_bash_completion "${HOME}"/.es-query/
-                        fi
-                else 
-                        echo "Differences found on es-query_bash_completion file."
-                        echo  -p "Do you want to update? Press ENTER to continue"
-                        mv "${HOME}"/.es-query/es-query_bash_completion{,.old}
-                        cp es-query_bash_completion "${HOME}"/.es-query/
-                        echo "old es-query_bash_completion was moved to .old"
-                        echo "new es-query_bash_completion installed"
-                fi
-        fi
+        rm "${HOME}"/bin/es-query.old
+        echo ""
+        echo "Update Done"
+        echo ""
+        echo "======================================================"
         echo "es-query Successfully updated"
-        echo "All Done!!"
+        checkDir
+        echo ""
+        echo "Checking associated files"
 fi
-
